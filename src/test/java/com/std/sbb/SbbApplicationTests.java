342ae39084d79274ef85a2af4ed7fb361212e2cf
ko <139303842+rhkddhks18@users.noreply.github.com>
2024-02-28 19:12:38 +0900

    테스트 주석

--- src/test/java/com/std/sbb/SbbApplicationTests.java
+++ src/test/java/com/std/sbb/SbbApplicationTests.java
@@ -21,69 +21,69 @@ import java.util.Optional;
 
 @SpringBootTest
 class SbbApplicationTests {
-	@Autowired
-	private QuestionRepository questionRepository;
-	@Autowired
-	private AnswerRepository answerRepository;
-	@Test
-	@DisplayName("질문")
-	void test001() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);  // 첫번째 질문 저장
-
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);  // 두번째 질문 저장
-	}
-	@Test
-	@DisplayName("전체 조회")
-	void testJpa() {
-		List<Question> all = this.questionRepository.findAll();
-		assertEquals(2, all.size());
-
-		Question q = all.get(0);
-		assertEquals("sbb가 무엇인가요?", q.getSubject());
-	}
-
-	@Test
-	@DisplayName("데이터 수정하기")
-	void test007() {
-		Optional<Question> oq = this.questionRepository.findById(1);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-		q.setSubject("수정된 제목");
-		this.questionRepository.save(q);
-	}
-
-	@Test
-	@DisplayName("데이터 삭제하기")
-	void test008() {
-		assertEquals(2, this.questionRepository.count());
-		Optional<Question> oq = this.questionRepository.findById(1);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-		this.questionRepository.delete(q);
-		assertEquals(1, this.questionRepository.count());
-	}
-
-	@Test
-	@DisplayName("답변")
-	void test009() {
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-
-		Answer a = new Answer();
-		a.setContent("네 자동으로 생성됩니다.");
-		a.setQuestion(q);
-		a.setCreateDate(LocalDateTime.now());
-		this.answerRepository.save(a);
-	}
+//	@Autowired
+//	private QuestionRepository questionRepository;
+//	@Autowired
+//	private AnswerRepository answerRepository;
+//	@Test
+//	@DisplayName("질문")
+//	void test001() {
+//		Question q1 = new Question();
+//		q1.setSubject("sbb가 무엇인가요?");
+//		q1.setContent("sbb에 대해서 알고 싶습니다.");
+//		q1.setCreateDate(LocalDateTime.now());
+//		this.questionRepository.save(q1);  // 첫번째 질문 저장
+//
+//		Question q2 = new Question();
+//		q2.setSubject("스프링부트 모델 질문입니다.");
+//		q2.setContent("id는 자동으로 생성되나요?");
+//		q2.setCreateDate(LocalDateTime.now());
+//		this.questionRepository.save(q2);  // 두번째 질문 저장
+//	}
+//	@Test
+//	@DisplayName("전체 조회")
+//	void testJpa() {
+//		List<Question> all = this.questionRepository.findAll();
+//		assertEquals(2, all.size());
+//
+//		Question q = all.get(0);
+//		assertEquals("sbb가 무엇인가요?", q.getSubject());
+//	}
+//
+//	@Test
+//	@DisplayName("데이터 수정하기")
+//	void test007() {
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//		q.setSubject("수정된 제목");
+//		this.questionRepository.save(q);
+//	}
+//
+//	@Test
+//	@DisplayName("데이터 삭제하기")
+//	void test008() {
+//		assertEquals(2, this.questionRepository.count());
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//		this.questionRepository.delete(q);
+//		assertEquals(1, this.questionRepository.count());
+//	}
+//
+//	@Test
+//	@DisplayName("답변")
+//	void test009() {
+//		Optional<Question> oq = this.questionRepository.findById(2);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//
+//		Answer a = new Answer();
+//		a.setContent("네 자동으로 생성됩니다.");
+//		a.setQuestion(q);
+//		a.setCreateDate(LocalDateTime.now());
+//		this.answerRepository.save(a);
+//	}
 //	@Autowired
 //	private QuestionService questionService;
 //	@Test
@@ -94,14 +94,14 @@ class SbbApplicationTests {
 //			this.questionService.create(subject, content);
 //		}
 //	}
-
-	@Autowired
-	private UserService userService;
-
-	@Test
-	@DisplayName("회원데이터 넣기")
-	void beforeEachUserData() {
-		userService.create("user2", "user2@test.com", "1234");
-		userService.create("user3", "user3@test.com", "1234");
-	}
+//
+//	@Autowired
+//	private UserService userService;
+//
+//	@Test
+//	@DisplayName("회원데이터 넣기")
+//	void beforeEachUserData() {
+//		userService.create("user2", "user2@test.com", "1234");
+//		userService.create("user3", "user3@test.com", "1234");
+//	}
 }
